f703cac6a8cc77e8795746909c77f28d0fb452d2
Shahbaz Vidicraze <shahbazvidicraze@gmail.com>
2025-07-30 01:38:01 +0500

    30-07-2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"31c4875c7aa0079ae11a4850e2d06bd83d0019
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3337884927"
+    serviceWorkerVersion: "3913942028"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "942325cb4d92251ef534b0134634b3aa",
+const RESOURCES = {"flutter_bootstrap.js": "0cd5450441466139964b3bbae897c84a",
 "version.json": "1ad2f6a241abb37aa26e4d39540a0d8e",
 "index.html": "f8d5fea4030700a1b1579f5d32d2009d",
 "/": "f8d5fea4030700a1b1579f5d32d2009d",
-"main.dart.js": "928c4ba28931f0f1e48c3febdea99f88",
+"main.dart.js": "5a2c0af31b1ba23e15984eafc5d0b96b",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "2984cdf0121062bf583abe1df529448b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
