179487471f2189fb92b4da11e58435d44754e795
Shahbaz Vidicraze <shahbazvidicraze@gmail.com>
2025-07-31 07:54:25 +0500

    31-07-2025

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0cd5450441466139964b3bbae897c84a",
+const RESOURCES = {"flutter_bootstrap.js": "9856b7aa1861a2ceb58eafa10643c234",
 "version.json": "1ad2f6a241abb37aa26e4d39540a0d8e",
 "index.html": "f8d5fea4030700a1b1579f5d32d2009d",
 "/": "f8d5fea4030700a1b1579f5d32d2009d",
-"main.dart.js": "5a2c0af31b1ba23e15984eafc5d0b96b",
+"main.dart.js": "db84af510ff6011e8c7caf3f69d94d7f",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "2984cdf0121062bf583abe1df529448b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
